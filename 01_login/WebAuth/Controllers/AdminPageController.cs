6ff5975a136b5221c4d9c4ca1bfadb41d42cef6c
Mauro Bennici <mauro@youaremyguide.com>
2020-06-02 19:10:49 +0200

    login first test

---
+++ 01_login/WebAuth/Controllers/AdminPageController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAuth.Controllers
+{
+    [Authorize]
+    public class AdminPageController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ 01_login/WebAuth/Controllers/LoginController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Mvc;
+using WebAuth.Models;
+
+namespace WebAuth.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Index(LoginViewModel loginViewModel)
+        {
+            if (!ModelState.IsValid)  
+                return View(loginViewModel);
+
+            if (loginViewModel.Username == "admin" && loginViewModel.Password == "admin")
+            {
+                // you are an admin ;)
+
+                var claims = new List<Claim>();
+                claims.Add(new Claim(ClaimTypes.Name, "mybeautifulemail@mail.mail"));
+                claims.Add(new Claim(ClaimTypes.NameIdentifier, "123"));
+
+                var claimsIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+                var authProperties = new AuthenticationProperties
+                {
+                    AllowRefresh = true,
+                    // Refreshing the authentication session should be allowed.
+
+                    IsPersistent = false,
+                    // Whether the authentication session is persisted across 
+                    // multiple requests. When used with cookies, controls
+                    // whether the cookie's lifetime is absolute (matching the
+                    // lifetime of the authentication ticket) or session-based.
+                };
+
+                await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new ClaimsPrincipal(claimsIdentity), authProperties);
+                return RedirectToAction("Index", "AdminPage");
+            }
+
+            ModelState.AddModelError("", "Invalid Credentials");
+            return View(loginViewModel);
+        }
+
+        public async Task<IActionResult> LogOff()
+        {
+            await HttpContext.SignOutAsync();
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
